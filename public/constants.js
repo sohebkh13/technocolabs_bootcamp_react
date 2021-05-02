fe8f71924cdb99876d45080c4a491e5d8662d294
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-05-02 23:20:02 +0530

    Create constants.js

---
+++ public/constants.js
@@ -0,0 +1,15 @@
+const API_URL = {
+    COURSE_DETAIL : "http://localhost/Web-Development-Bootcamp-Project-07-March/api/course-detail.php"
+}
+
+const APP_URL = {
+    LOGIN : "/login",
+    REGISTER : "/register",
+    COURSE_DETAIL : "/course-detail",
+    BOOK_DETAIL : "/book-detail"
+}
+
+export {
+    API_URL,
+    APP_URL
+}
